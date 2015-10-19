a614fd20ab8117d32187710fd662dbcf93057748
mshava <sakhile@projectcodex.co>
2015-10-19 13:20:04 +0200

    routes/applicant.js, app.js

---
+++ Routes/applicant.js
@@ -0,0 +1,66 @@
+exports.showContracts = function (req, res, next) {
+	req.getConnection(function (err, connection) {
+		if (err)
+			return next (err);
+		var query = "SELECT * FROM Contracts";
+		connection.query(query, [], function (err, results) {
+			if (err){
+				return next(err);
+			};
+			res.render('Contracts',{
+				Contracts:results;
+			});
+		});
+	});
+};
+exports.addContracts = function (req, res, next) {
+	req.getConnection(function (err, connection) {
+		if (err)
+			return next(err);
+		var input = JSON.parse(JSON.stringify(req.body));
+		var data = {
+			Freelancer_name : input.Freelancer_name,
+			Company_placed : input.Company_placed,
+			Start_date : input.Start_date,
+			End_date : input.End_date,
+			Consultant : input.Consultant
+		};
+		var query = "insert into Contracts set ?";
+		connection.query(query, data, function (err, results) {
+			if (err)
+				console.log("Error inserting : %s ", err);
+			res.redirect("/Contracts");
+		});
+	});
+};
+exports.getContracts = function (req, res, next) {
+	var id = req.parse.id;
+	req.getConnection(function (err, connection) {
+		var query = "SELECT * FROM Contracts WHERE id = ?";
+		connection.query(query, [id], function (err, rows){
+			if (err){
+				console.log("Error Selecting : %s ", err);
+			};
+		var query = "SELECT * FROM Contracts";
+		connection.query(query,[id], function (err, Contracts){
+			if (err){
+				console.log('Contracts');
+				console.log("Error Selecting : %s ", err);
+			};
+				res.render('edit_Contracts',{page_page:'Edit Customers - Node.js',Contracts:Contracts,data: rows[0]});
+			});	
+		});
+	});
+};
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express'),
+	app = express();
+	exphbs = require('express-handlebars'),
+	mysql = require('mysql'),
+	bodyParser = require("body-parser"),
+	//myConnection = require("express-myconnection");
+
+
+
+
+	 
+
+/*
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
+*/
+
+//setup template handlebars as the template engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
+//setup middleware
+//app.use(myConnection(mysql, dbOptions, 'single'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+
+app.listen(3000, function () {
+    console.log('express-handlebars example server listening on: 3000');
+});
